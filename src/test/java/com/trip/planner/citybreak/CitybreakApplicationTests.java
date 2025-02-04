5c57dce44e1a527e11768e0e8b8a109266af22aa
DanielTerteci <terteci.daniel@yahoo.com>
2025-02-05 01:35:17 +0200

    initial commit

---
+++ src/test/java/com/trip/planner/citybreak/CitybreakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.planner.citybreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
